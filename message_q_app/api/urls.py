47cda7e726caaf1ae9f6e956c7ec701d2f269bb9
DilipJ98 <dilip@cklabs.in>
2025-01-26 15:03:12 +0530

    removed: api/urls.py

--- message_q_app/api/urls.py
+++
@@ -1,9 +0,0 @@
-# message_q_app/api/urls.py
-from django.urls import path
-from ..views import test_view
-
-app_name = 'message_q_app'  # This should match PluginURLs.APP_NAME
-
-urlpatterns = [
-    path('test/', test_view, name='test'),
-]
--- message_q_app/urls.py
+++ message_q_app/urls.py
@@ -1,25 +1,14 @@
-# """
-# URLs for message_q_app.
-# """
-# from django.urls import path 
-# from django.views.generic import TemplateView
-
-# urlpatterns = [
-#     path('', TemplateView.as_view(template_name="message_q_app/base.html")),
-# ]
-
-# message_q_app/urls.py
-from django.urls import path, include
+from django.urls import path
+from .views import test_view  # Import your views
 from django.views.generic import TemplateView
 
 
 app_name = 'message_q_app'
 
 urlpatterns = [
-
-    # Non-API URLs
+    path('test/', test_view, name='test'),  # Your endpoint
     path('', TemplateView.as_view(template_name="message_q_app/base.html"), name='index'),
-
-    # Include the API URLs from api/urls.py
-    path('api/', include('message_q_app.api.urls')),  # This will map /api/ to your API urls
 ]
+
+
+
