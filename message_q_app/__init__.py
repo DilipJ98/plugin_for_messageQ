4d9e0c14e3108095ac54f4f074e8e07717803098
DilipJ98 <dilip@cklabs.in>
2025-01-27 16:26:10 +0530

    added: migrations

--- message_q_app/__init__.py
+++ message_q_app/__init__.py
@@ -10,3 +10,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'message_q_app.settings')
 __version__ = '0.1.0'
 
 default_app_config = 'message_q_app.apps.MessageQAppConfig' 
+# Expose key functions, views, etc.
+from .views import test_view  # Expose test_view for direct import
+from .urls import urlpatterns  # Expose urlpatterns if necessary
+
+__all__ = ['test_view', 'urlpatterns']
