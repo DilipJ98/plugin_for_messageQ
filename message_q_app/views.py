731c30df50b1abdc4254a6e5a82aee7f035b248d
DilipJ98 <dilip@cklabs.in>
2025-01-30 12:55:01 +0530

    added: xbnlock thing to plugin

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -1,5 +1,13 @@
 from django.http import JsonResponse
+from xblock.core import XBlock
+from xblock.runttime import Runtime
+
+
 
 def test_view(request):
-    print('Message Queue App is working!............................................!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    return JsonResponse({'message': 'Message Queue App is working!'})
+
+    usage_key = "e34b28140bb048a7aa6eac8d0f13e6a9"
+    runtime = Runtime()
+    xblock_instance = runtime.get_block(usage_key)
+    print(xblock_instance, " this is xblock instance..............1.................1111!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
+    return JsonResponse({'message': xblock_instance})
