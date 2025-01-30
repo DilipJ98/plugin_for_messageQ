e3cadf06ce880fe5fc95b56d57df213d16ee090c
DilipJ98 <dilip@cklabs.in>
2025-01-30 15:57:19 +0530

    added: module store to views.py

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -1,13 +1,10 @@
 from django.http import JsonResponse
-from xblock.core import XBlock
-from xblock.runtime import Runtime
-
-
+from xmodule.modulestore.django import modulestore
+from opaque_keys.edx.keys import UsageKey
 
 def test_view(request):
-
-    usage_key = "e34b28140bb048a7aa6eac8d0f13e6a9"
-    runtime = Runtime()
-    xblock_instance = runtime.get_block(usage_key)
-    print(xblock_instance, " this is xblock instance..............1.................1111!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
-    return JsonResponse({'message': xblock_instance})
+    location = "block-v1:cklabs+XBLOCK002+202_T1+type@textxblock+block@e34b28140bb048a7aa6eac8d0f13e6a9"
+    usage_key = UsageKey.from_string(location)
+    xblock_instance = modulestore().get_item(usage_key)
+    print(xblock_instance, " this is xblock instance..........1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1..11.1.1.1.1.1.1.1.1..1")
+    return JsonResponse({'message': "api working"})
