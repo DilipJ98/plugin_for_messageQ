95e810fba27118c081783043f342eccb498ecd16
DilipJ98 <dilip@cklabs.in>
2025-01-31 10:21:28 +0530

    updating marks filed

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -6,17 +6,12 @@ def test_view(request):
     location = "block-v1:cklabs+XBLOCK002+202_T1+type@textxblock+block@e34b28140bb048a7aa6eac8d0f13e6a9"
     usage_key = UsageKey.from_string(location)
     xblock_instance = modulestore().get_item(usage_key)
-
-    print(xblock_instance, " this is xblock instance..........1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1..11.1.1.1.1.1.1.1.1..1")
-    print(xblock_instance.boilerplate_code, " this is boilderplate code")
-    xblock_instance.boilerplate_code = "this is new boilerplate code from api"
+    print(xblock_instance.marks, "intial marks of the student")
+    xblock_instance.marks = 3
     try:
         modulestore().update_item(xblock_instance, 8)
         print("try executing after update method in try")
     except Exception as e:
-        print("catch executing")
         print(e)
-        modulestore().update_item(xblock_instance, '8')
-        print("after save")
         return JsonResponse({'message': f"Error while updating item: {e}"})
     return JsonResponse({'message': "api working"})
