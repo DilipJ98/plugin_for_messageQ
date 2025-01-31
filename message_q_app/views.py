d197e002dc6839f8732c365681f07967bb1107cb
DilipJ98 <dilip@cklabs.in>
2025-01-31 10:43:01 +0530

    added: code to get the method from xblock

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -3,12 +3,15 @@ from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import UsageKey
 
 def test_view(request):
-    location = "block-v1:cklabs+XBLOCK002+202_T1+type@textxblock+block@e34b28140bb048a7aa6eac8d0f13e6a9"
-    usage_key = UsageKey.from_string(location)
-    xblock_instance = modulestore().get_item(usage_key)
-    print(xblock_instance.marks, "intial marks of the student")
-    xblock_instance.marks = 3
     try:
+        location = "block-v1:cklabs+XBLOCK002+202_T1+type@textxblock+block@e34b28140bb048a7aa6eac8d0f13e6a9"
+        usage_key = UsageKey.from_string(location)
+        xblock_instance = modulestore().get_item(usage_key)
+        print(xblock_instance, " this is xblock instance.1.1.1.1.11.1..11..1.1111111111111111111111111111111111")
+        print(xblock_instance.marks, "intial marks of the student")
+        result = xblock_instance.database_connection_fun()
+        print(result, "this is result of the database connection............................................")
+        xblock_instance.marks = 3
         modulestore().update_item(xblock_instance, 8)
         print("try executing after update method in try")
     except Exception as e:
