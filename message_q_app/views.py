31112d34ac8177a2773542587f560ed23c5c7be5
DilipJ98 <dilip@cklabs.in>
2025-02-02 16:35:02 +0530

    added: print statments in views

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -12,22 +12,27 @@ def test_view(request):
         body = json.loads(request.body)
         submission_id = body.get('x-submission-id')
         redis_data = redis_client.hgetall(submission_id)
+        print("inside try before redis")
         if redis_data:
+            print("inside try after redis")
             usage_key_from_redis = redis_data.get("usage_key")
             student_id_from_redis = redis_data.get("student_id")
             location = "block-v1:cklabs+XBLOCK002+202_T1+type@textxblock+block@"+usage_key_from_redis
             usage_key = UsageKey.from_string(location)
             xblock_instance = modulestore().get_item(usage_key)
+            print("after getting xblock instance............................................")
             xblock_instance.marks = 10
             xblock_instance.boilerplate_code = "boilerplate code"
             score = body.get('score')
             is_correct = body.get('is_correct')
             message = body.get('message')
+            print("before update method............................................")                
             result = xblock_instance.update_grades_of_student(score, is_correct, message)
             modulestore().update_item(xblock_instance, student_id_from_redis)
+            print("after update method............................................")
             print(result, "this is result of grade funtion............................................")
             print("try executing after update method in try")
     except Exception as e:
-        print(e)
+        print(e, "  something is woring in the catch block so exec block is executing..................################################3")
         return JsonResponse({'message': f"Error while updating item: {e}"})
     return JsonResponse({'message': "api working"})
