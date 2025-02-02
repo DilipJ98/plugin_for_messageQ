54beffdc2e33572b8a6c44c57a84a27ff8d73bbe
DilipJ98 <dilip@cklabs.in>
2025-02-02 11:42:18 +0530

    refactored: code in views.py

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -15,18 +15,16 @@ def test_view(request):
         if redis_data:
             usage_key_from_redis = redis_data.get("usage_key")
             student_id_from_redis = redis_data.get("student_id")
-
             location = "block-v1:cklabs+XBLOCK002+202_T1+type@textxblock+block@"+usage_key_from_redis
             usage_key = UsageKey.from_string(location)
             xblock_instance = modulestore().get_item(usage_key)
             xblock_instance.marks = 10
             xblock_instance.boilerplate_code = "boilerplate code"
-            xblock_instance.score = body.get('score')
-            xblock_instance.is_correct = body.get('is_correct')
-            xblock_instance.message = body.get('message')
-
+            score = body.get('score')
+            is_correct = body.get('is_correct')
+            message = body.get('message')
+            result = xblock_instance.update_grades_of_student(score, is_correct, message)
             modulestore().update_item(xblock_instance, student_id_from_redis)
-            result = xblock_instance.update_grades_of_student()
             print(result, "this is result of grade funtion............................................")
             print("try executing after update method in try")
     except Exception as e:
