48fa902a03609f18c7f7797d221dce5f23640825
DilipJ98 <dilip@cklabs.in>
2025-02-03 15:20:49 +0530

    added: student module

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import UsageKey
+from lms.djangoapps.courseware.models import StudentModule
+
 import redis
 import json
 
@@ -25,9 +27,20 @@ def test_view(request):
             score = body.get('score')
             is_correct = body.get('is_correct')
             message = body.get('message')
-            results = xblock_instance.update_grades_of_student(score, is_correct, message, student_id_from_redis)
+            results = xblock_instance.update_grades_of_student()
             print(results, " resulsts from update fun ##############################")
             modulestore().update_item(xblock_instance, student_id_from_redis)
+            print("data saved in modulestore")
+            
+            #student module
+            student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
+            state = json.loads(student_module.state)
+            state['score'] = score
+            state['message'] = message
+            student_module.state = json.dumps(state)
+            student_module.save()
+
+            print("data saved in student module")
             print("try executing after update method in try")
     except Exception as e:
         print(e, "  something is woring in the catch block so exec block is executing..................################################3")
