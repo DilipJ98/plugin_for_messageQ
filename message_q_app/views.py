eaeb9adb69c12dc42d46629571ed6bd550371e11
DilipJ98 <dilip@cklabs.in>
2025-02-04 14:20:34 +0530

    added: student module

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import UsageKey
 from lms.djangoapps.courseware.models import StudentModule
-from lms.djangoapps.courseware.user_state_client import XBlockUserStateClient
+# from lms.djangoapps.courseware.user_state_client import XBlockUserStateClient
 import traceback
 
 import redis
@@ -36,45 +36,46 @@ def test_view(request):
             modulestore().update_item(xblock_instance, student_id_from_redis)
             print("data saved in modulestore")
             
-            #xblockuser state client
-            state_client = XBlockUserStateClient()
-            print("state client object created.............")
-            existing_state = state_client.get(student_id_from_redis, usage_key) or {}
-            print(existing_state, " existing state from xblockuserstateclient from views####")
-            if not existing_state:
-                print("No existing state found, creating a new one.")
-                existing_state = {"score": 0, "is_correct": False, "message": ""}
-            existing_state["score"] = score
-            existing_state["is_correct"] = is_correct
-            existing_state["message"] = message
-            print("daat assigned to existing state............")
-            state_client.set(student_id_from_redis, usage_key, existing_state)
-            print("data saved in xblockuserstateclient")
+            # #xblockuser state client
+            # state_client = XBlockUserStateClient()
+            # print("state client object created.............")
+            # existing_state = state_client.get(student_id_from_redis, usage_key) or {}
+            # print(existing_state, " existing state from xblockuserstateclient from views####")
+            # if not existing_state:
+            #     print("No existing state found, creating a new one.")
+            #     existing_state = {"score": 0, "is_correct": False, "message": ""}
+            # existing_state["score"] = score
+            # existing_state["is_correct"] = is_correct
+            # existing_state["message"] = message
+            # print("daat assigned to existing state............")
+            # state_client.set(student_id_from_redis, usage_key, existing_state)
+            # print("data saved in xblockuserstateclient")
 
-            #retrieve data from xblockuserstateclient
-            user_state = state_client.get(student_id_from_redis, usage_key)
-            print("data retrieved from xblockuserstateclient")
-            print(user_state.get('score'), user_state.get('is_correct'), user_state.get('message'), " user state from xblockuserstateclient from views####")
+            # #retrieve data from xblockuserstateclient
+            # user_state = state_client.get(student_id_from_redis, usage_key)
+            # print("data retrieved from xblockuserstateclient")
+            # print(user_state.get('score'), user_state.get('is_correct'), user_state.get('message'), " user state from xblockuserstateclient from views####")
 
 
             # #student module
-            # student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
-            # state = json.loads(student_module.state)
-            # state['score'] = score
-            # state['message'] = message
-            # student_module.state = json.dumps(state)
-            # student_module.save()
-            # print("data saved in student module")
+            student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
+            state = json.loads(student_module.state)
+            state['score'] = score
+            state['message'] = message
+            student_module.state = json.dumps(state)
+            student_module.save()
+            print("data saved in student module")
 
-            # #get updated values
-            # updated_student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
-            # updated_state = json.loads(updated_student_module.state)
-            # print(updated_state.get('score'), updated_state.get('message'), " updated state from student module")
+            #get updated values
+            updated_student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
+            updated_state = json.loads(updated_student_module.state)
+            print(updated_state.get('score'), updated_state.get('message'), " updated state from student module")
             
+            #update grades method of xblock
             # results = xblock_instance.update_grades_of_student(student_id_from_redis, usage_key)
             # print(results, " resulsts from update fun ##############################")
             
-            # print("try executing after update method in try")
+            print("try executing after update method in try")
     except Exception as e:
         print( type(e), e,"  :something is woring in the catch block so exec block is executing..................################################3")
         traceback.print_exc()
