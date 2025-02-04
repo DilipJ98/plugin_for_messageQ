158884e3d00e5645f0d6b9dd2a4ef0d797f8e920
DilipJ98 <dilip@cklabs.in>
2025-02-04 08:21:05 +0530

    added: xblock user state client

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import UsageKey
 from lms.djangoapps.courseware.models import StudentModule
+from lms.djangoapps.courseware.models import XBlockUserStateClient
+
 
 import redis
 import json
@@ -31,24 +33,39 @@ def test_view(request):
             modulestore().update_item(xblock_instance, student_id_from_redis)
             print("data saved in modulestore")
             
-            #student module
-            student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
-            state = json.loads(student_module.state)
-            state['score'] = score
-            state['message'] = message
-            student_module.state = json.dumps(state)
-            student_module.save()
-            print("data saved in student module")
+            #xblockuser state client
+            state_client = XBlockUserStateClient()
+            existing_state = state_client.get(student_id_from_redis, usage_key) or {}
+            existing_state["score"] = score
+            existing_state["is_correct"] = is_correct
+            existing_state["message"] = message
 
-            #get updated values
-            updated_student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
-            updated_state = json.loads(updated_student_module.state)
-            print(updated_state.get('score'), updated_state.get('message'), " updated state from student module")
+            state_client.set(student_id_from_redis, usage_key, existing_state)
+
+
+            #retrieve data from xblockuserstateclient
+            user_state = state_client.get(student_id_from_redis, usage_key)
+            print(user_state.get('score'), user_state.get('is_correct'), user_state.get('message'), " user state from xblockuserstateclient from views####")
+
+
+            # #student module
+            # student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
+            # state = json.loads(student_module.state)
+            # state['score'] = score
+            # state['message'] = message
+            # student_module.state = json.dumps(state)
+            # student_module.save()
+            # print("data saved in student module")
+
+            # #get updated values
+            # updated_student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
+            # updated_state = json.loads(updated_student_module.state)
+            # print(updated_state.get('score'), updated_state.get('message'), " updated state from student module")
             
-            results = xblock_instance.update_grades_of_student(student_id_from_redis, usage_key)
-            print(results, " resulsts from update fun ##############################")
+            # results = xblock_instance.update_grades_of_student(student_id_from_redis, usage_key)
+            # print(results, " resulsts from update fun ##############################")
             
-            print("try executing after update method in try")
+            # print("try executing after update method in try")
     except Exception as e:
         print(e, "  something is woring in the catch block so exec block is executing..................################################3")
         return JsonResponse({'message': f"Error while updating item: {e}"})
