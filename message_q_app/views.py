fc5a8ecd27abcc9d37a6b500e7eceb383a360d57
DilipJ98 <dilip@cklabs.in>
2025-02-05 12:13:12 +0530

    added: message queue method

--- message_q_app/views.py
+++ message_q_app/views.py
@@ -11,33 +11,30 @@ from celery import shared_task
 
 redis_client = redis.StrictRedis(host='host.docker.internal', port=6379, db=0, decode_responses=True)
 
-
-
-def test_view(request):
+@shared_task(queue="edx.lms.core.default")
+def test_view(message):
+    print("test_view called..###!!@@@#####@@@@@$$$$$$$$$")
     try:
-        body = json.loads(request.body)
-        submission_id = body.get('x-submission-id')
+        submission_id = message.get('x-submission-id')
         redis_data = redis_client.hgetall(submission_id)
-        print("inside try before redis")
         if redis_data:
-            print("inside try after redis")
+            #getting the usage_key and student_id from redis
             usage_key_from_redis = redis_data.get("usage_key")
             student_id_from_redis = redis_data.get("student_id")
             location = "block-v1:cklabs+XBLOCK002+202_T1+type@textxblock+block@"+usage_key_from_redis
             usage_key = UsageKey.from_string(location)
-            print(f"Usage Key from Redis: {usage_key_from_redis}")
-            print(f"Student ID from Redis: {student_id_from_redis}")
-            print(f"usage key: {usage_key}")
+            
+            #for xblock scop type content 
             xblock_instance = modulestore().get_item(usage_key)
             xblock_instance.marks = 10
             xblock_instance.boilerplate_code = "boilerplate code"
-            score = body.get('score')
-            is_correct = body.get('is_correct')
-            message = body.get('message')
-
             modulestore().update_item(xblock_instance, student_id_from_redis)
-            print("data saved in modulestore")
             
+            #for xblock user specific details
+            score = message.get('score')
+            is_correct = message.get('is_correct')
+            message = message.get('message')
+
             # #student module
             student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
             state = json.loads(student_module.state)
@@ -46,17 +43,9 @@ def test_view(request):
             state['is_correct'] = is_correct
             student_module.state = json.dumps(state)
             student_module.save()
-            print("data saved in student module")
 
     except Exception as e:
         print( type(e), e)
         traceback.print_exc()
         return JsonResponse({'message': f"Error while updating item: {e}"})
     return JsonResponse({'message': "api working"})
-
-
-@shared_task(queue="edx.lms.core.default")
-def listen(data):
-    print("Hurray inside listen###########")
-    print(data, " data from messageQ")
-    return data
