722ab1ee3cc17682c32c281166bec09c52ca3725
DilipJ98 <dilip@cklabs.in>
2025-02-06 16:29:48 +0530

    added: another method

--- message_q_app/urls.py
+++ message_q_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import test_view  # Import your views
+from .views import test_view, for_api
 from django.views.generic import TemplateView
 
 
 app_name = 'message_q_app'
 
 urlpatterns = [
-    path('test/', test_view, name='test'),  # Your endpoint
+    path('test/', test_view, name='test'),
+    path('test2/', for_api, name='test2') , 
     path('home', TemplateView.as_view(template_name="message_q_app/base.html"), name='index'),
 ]
 
--- message_q_app/views.py
+++ message_q_app/views.py
@@ -9,23 +9,19 @@ import redis
 import json
 from celery import shared_task
 
+from lms.djangoapps.grades.models import Score
+from student.models import CourseEnrollment
+from django.contrib.auth.models import User
+from django.db import transaction
+
+
 redis_client = redis.StrictRedis(host='host.docker.internal', port=6379, db=0, decode_responses=True)
 
 @shared_task(queue="edx.lms.core.default")
 def test_view(message_queue):
-    print("test_view called..###!!@@@#####@@@@@$$$$$$$$$")
-    print(type(message_queue), " this is type of messageQQQQQQQQQQQQQQQQQQQQQQQQQ")
-    print(message_queue, " this is message QQQQQQQQQQQQQQQ")
-
-
+    print("test_view called")
     try:
-        if isinstance(message_queue, str):
-            message_queue = json.loads(message_queue)
-        if isinstance(message_queue, list):
-            payload = message_queue[0]
-        else:
-            payload = message_queue 
-
+        payload = message_queue 
         submission_id = payload.get('x-submission-id')
         redis_data = redis_client.hgetall(submission_id)
         if redis_data:
@@ -45,9 +41,7 @@ def test_view(message_queue):
             score = payload.get('score')
             is_correct = payload.get('is_correct')
             message = payload.get('message')
-            print(payload, " message...................")
-            print("score", score, is_correct, message, " from test_view..........................###")
-
+            
             # #student module
             student_module = StudentModule.objects.get(student_id=student_id_from_redis, module_state_key=usage_key)
             state = json.loads(student_module.state)
@@ -63,3 +57,35 @@ def test_view(message_queue):
         traceback.print_exc()
         return JsonResponse({'message': f"Error while updating item: {e}"})
     return JsonResponse({'message': "api working"})
+
+
+
+def for_api(request):
+    try:
+        data = json.loads(request.body.decode("utf-8"))
+        submission_id = data.get('x-submission-id')
+        redis_data = redis_client.hgetall(submission_id)
+        if redis_data:
+            usage_key_from_redis = redis_data.get("usage_key")
+            student_id_from_redis = redis_data.get("student_id")
+            location = "block-v1:cklabs+XBLOCK002+202_T1+type@textxblock+block@"+usage_key_from_redis
+            usage_key = UsageKey.from_string(location)
+            student = User.objects.get(id=student_id_from_redis)
+            with transaction.atomic():
+                student_module, created = StudentModule.objects.update_or_create(
+                student=student,
+                module_state_key=usage_key,  
+                defaults={
+                    "grade": data.get("score"),           
+                    "max_grade": data.get("maxscore")    
+                }
+            )
+            return JsonResponse({'message': 'success'}, status = 200)
+        return JsonResponse({'message': 'submission id missing in redis'}, status = 404)
+    except json.JSONDecodeError:
+        return JsonResponse({'message' : 'Invalid json'}, status = 400)
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        return JsonResponse({'message': e}, status = 500)
+    
